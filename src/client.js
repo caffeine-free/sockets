9c41765e0752770463669eddd8c04be575d26309
Gabriel Araújo <gabrielcaetanodm@gmail.com>
2021-03-30 22:15:37 -0300

    feat: new new feature

--- src/client.js
+++ src/client.js
@@ -19,23 +19,27 @@ client.on('connect', function() {
 		}
 	}
 
-  	console.log('Client: connection established with server');
+	console.log('Client: connection established with server');
 
-    var address = client.address();
-    var port = address.port;
-    console.log('Client is listening at port: ' + port);
+	var address = client.address();
+	var port = address.port;
+	console.log('Client is listening at port: ' + port);
 
 	client.write(operations.shift());	
 
-    client.on('data', function(data) {
+	client.on('data', function(data) {
 		data = JSON.parse(data);
 		if (data.output) {
 			var object = {
-				"input" : operations.shift()
+				"input" : operations.shift(),
+				"client" : data.client
 			}
+
+			console.log(data.output);
 			client.write(JSON.stringify(object));
 		}
 		else if (data.input) {
+			console.log(data.input);
 			var regC = /[b-df-hj-np-tv-z]/gi;
 			var regV = /[aeiou]/gi;
 			var regN = /\d/gi;
@@ -44,30 +48,31 @@ client.on('connect', function() {
 			var vowels = data.input.match(regV)?.length;
 			var numbers = data.input.match(regN)?.length
 			
-			if (consoants == undefined) {
+			if (!consoants) {
 				consoants = 0;
 			}
-			if (vowels == undefined) {
+			if (!vowels) {
 				vowels = 0;
 			}
-			if (numbers == undefined) {
+			if (!numbers) {
 				numbers = 0;
 			}
 
 			if (consoants == 0 && vowels == 0 && numbers == 0) {
 				var object = {
-					"output" : "erro"
+					"output" : "erro",
+					"client" : data.client
 				}
 				client.write(JSON.stringify(object));
 			}
 			else {
 				var output = `C=${consoants};V=${vowels};N=${numbers}`;
 				var object = {
-					"output" : output
+					"output" : output,
+					"client" : data.client
 				}
 				client.write(JSON.stringify(object));
 			}
-
 		}
 	});
 });
--- src/server.js
+++ src/server.js
@@ -3,57 +3,60 @@ const server = net.createServer();
 var clients = new Map();
 
 server.on('connection', function (client) {     
-    client.setEncoding('utf8');
-    
-    var rport = client.remotePort;
-    
-    console.log('\nClient is listening at port: ' + rport);
-    
-    server.getConnections(function (error, count) {
-        console.log('Number of concurrent connections to the server : ' + count);
-    });
+  client.setEncoding('utf8');
+	
+	var rport = client.remotePort;
+	
+	console.log('\nClient is listening at port: ' + rport);
 
-    client.on('data', function (data) {
-        if (clients.get(client) == null) {
-            clients.set(client, data);
-            var object = {
-				"output" : "a"
-			}
-            client.write(JSON.stringify(object));
+	server.getConnections(function (error, count) {
+    console.log('Number of concurrent connections to the server : ' + count);
+	});
+
+	client.on('data', function (data) {
+    if (clients.get(client) == null) {
+      clients.set(client, data);
+      var object = {
+        "output" : "start",
+        "client": client
+	  	}
+      client.write(JSON.stringify(object));
+    }
+    else {
+      data = JSON.parse(data);
+      if (data.input) {
+        let keys = Array.from(clients.keys());
+        let randomClient = keys[Math.floor(Math.random() * keys.length)];
+        randomClient.write(JSON.stringify(data));
+      }
+      else if(data.output) {
+        var object = {
+          "output" : data.output,
+          "client" : data.client
         }
-        else {
-            data = JSON.parse(data);
-            if (data.input) {
-                var keys = Array.from(clients.keys());
-                var randomClient = keys[Math.floor(Math.random() * keys.length)];
-                randomClient.write(JSON.stringify(data));
-            }
-            else if(data.output) {
-                console.log(data);
-                var object = {
-                    "output" : "a"
-                }
-                client.write(JSON.stringify(object));
-            }
-        }
-    });
+        let keys = Array.from(clients.keys());
+        let client = keys.find(c => c.remotePort === data.client._peername.port);
+        client.write(JSON.stringify(object));
+      }
+    }
+	});
 
-    client.on('error', function (error) {
-      	console.log('Error : ' + error);
-    });
+	client.on('error', function (error) {
+    console.log('Error : ' + error);
+	});
 
-    client.on('close', function (error) {
-    	console.log('Socket closed!');
-    	if(error) {
-        	console.log(error);
-      	}
-    });  
+	client.on('close', function (error) {
+		console.log('Socket closed!');
+		if(error) {
+      console.log(error);
+    }
+	});
 });
 
 server.listen(4444, function() {
-    var address = server.address();
-    var port = address.port;
-    var family = address.family;
-    console.log('Server is listening at port: ' + port);
-    console.log('Server is IP4/IP6 : ' + family);
+	var address = server.address();
+	var port = address.port;
+	var family = address.family;
+	console.log('Server is listening at port: ' + port);
+	console.log('Server is IP4/IP6 : ' + family);
 });
